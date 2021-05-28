1b8a69dbae5beda2ddabf3ede529a0b263691f19
zhjmcp <1037194737@qq.com>
2021-05-28 17:47:12 +0800

    test

--- cloud-gateway-gateway9527/src/main/java/com/eiletxie/springcloud/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/eiletxie/springcloud/GatewayMain9527.java
@@ -13,6 +13,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GatewayMain9527 {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayMain9527.class,args);
+        SpringApplication.run(GatewayMain9527.class, args);
     }
 }
